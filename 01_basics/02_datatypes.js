3de56938dee232d231c79fc25d811dfc8a5e701c
aman12-AI <amanlenka413@gmail.com>
2025-07-22 07:42:13 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all js code as newer version
+
+// alert("hello");//we are using  nodejs,not browser
+//
+
+// in codinng most important is readability,it should be high 
+console.log(3+3);//using line break is agood practice;
+console.log("Hitesh")
+
+// datatypes
+let state;
+//number=> 2 to power 53
+//bigint
+//string=>""
+//boolean => true/false
+//null =>standalone value
+//undefined =>
+//symbol =>unique
+
+// non-primitive
+//object;
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
